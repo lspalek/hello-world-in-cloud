b7ec2cd1875f3f189fb30ab5e43dfcf2e1c80cb3
Lukas Spalek <spalek@epages.com>
2022-09-29 17:18:15 +0200

    init hello world app

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Hello & Welcome to Cloud App !!!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String greeting(@PathVariable String name) {
+        return "Hello `" + name + "`.";
+    }
+}
